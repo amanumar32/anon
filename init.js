e08a93eefa73cb59709fed5b7e8f11f3614d487d
Aa Man <amanumar2006@gmail.com>
2026-09-13 14:47:44 +0100

    minor fixes and improvements to the recache function in init.js, including ensuring directories exist, handling configuration file creation, and updating cache configurations based on the mode (start or update).

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 export const cache = {
     configs: {
         user: { name: '', number: '', id: '' },
-        bot: { name: '', version: 'v1.0.0' },
+        bot: { name: 'Anon', version: 'v1.0.0' },
         settings: { public: false, respond: false }
     }
 }
@@ -11,14 +11,15 @@ export const cache = {
 export async function recache(sock = null, mode = 'update') {
     const configPath = 'configs.json';
     const directories = ['./logs', './database'];
-    
+
     directories.forEach(dir => fs.mkdirSync(dir, { recursive: true }));
+    if (!fs.existsSync(configPath)) fs.writeFileSync(configPath, JSON.stringify(cache.configs, null, 4));
 
     if (mode === 'start') {
         const local_configs = JSON.parse(fs.readFileSync(configPath) || "{}");
         Object.keys(cache.configs).forEach(sec => Object.keys(cache.configs[sec]).forEach(val => cache.configs[sec][val] = local_configs[sec][val] || cache.configs[sec][val]));
         cache.configs.user.id = (sock?.user?.lid.split(':')[0] + '@lid') || cache.configs.user.id;
     } else {
-
+        fs.writeFileSync(configPath, JSON.stringify(cache.configs, null, 4));
     }
 }
