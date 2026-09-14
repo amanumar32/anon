863b3f09fbd7d028e2a1f29876b578624a64e854
Aa Man <amanumar2006@gmail.com>
2026-09-14 21:34:26 +0100

    test

--- library/functions.js
+++ library/functions.js
@@ -1,2 +1,1 @@
 export const sentence_case = (str = '') => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-export const clean_id = (id) => id ? id.split(':')[0].split('@')[0] : '';
