cc31123e5ffeed0336b635f50bf40fee52e13833
Aa Man <amanumar2006@gmail.com>
2026-09-18 18:50:35 +0100

    removed imgbb api and replace with tele.gh

--- commands/media.js
+++ commands/media.js
@@ -136,21 +136,21 @@ class Media {
         try {
             if (!_return) send.react(from, '🔄', msg.key);
             const media = quotedMsg || msg.message || null;
-            if (!media?.imageMessage && !media?.stickerMessage) throw new Error('Only images and stickers can be uploaded.');
-            if (!process.env.IMGBB_API_KEY) throw new Error('No `IMGBB_API_KEY` api key provided in `.env`');
+            if (!media?.imageMessage && !media?.videoMessage && !media?.stickerMessage) throw new Error('Only images, videos, and stickers can be uploaded.');
             const buffer = await downloadMediaMessage({ key: { remoteJid: from }, message: media }, 'buffer', {});
             const form = new FormData();
-            const mimetype = (media.stickerMessage || media.imageMessage)?.mimetype || 'image/jpeg';
+            const mimetype = (media.stickerMessage || media.imageMessage || media.videoMessage)?.mimetype || 'image/jpeg';
             const blob = new Blob([buffer], { type: mimetype });
-            const ext = mimetype?.split('/')[1].replace('jpeg', 'jpg') || 'jpg';
-            form.append('image', blob, `upload.${ext}`);
-            const response = await axios.post(`https://api.imgbb.com/1/upload?key=${process.env.IMGBB_API_KEY}`, form);
-            const url = response.data?.data?.display_url || '';
-            if (!url) throw new Error('No response from API.');
-            if (_return) return { url }
-            await send.text(from, `*Image URL:* ${url}`, msg);
+            const ext = mimetype?.split('/')[1]?.replace('jpeg', 'jpg') || 'jpg';
+            form.append('file', blob, `upload.${ext}`);
+            const response = await axios.post('https://telegra.ph/upload', form);
+            const file_path = response.data?.[0]?.src;
+            if (!file_path) throw new Error('No response from upload server.');
+            const url = `https://telegra.ph${file_path}`;
+            if (_return) return { url };
+            await send.text(from, `*Media URL:* ${url}`, msg);
         } catch (error) {
-            console.error('Image upload error:', error.message);
+            console.error('Media upload error:', error.message);
             if (_return) throw error;
             send.text(from, error.message, msg);
         }
